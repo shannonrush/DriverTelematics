e55e4fb2096367e565475dc8cfe8574611769f56
Shannon Rush <shannonmrush@gmail.com>
2015-02-27 13:49:00 -0700

    adding features: MaxDistanceFromStart, TotalDistanceTraveled, AverageTravelSpeed, TotalTimeTravel, TotalTimeStopped and associated tests

--- RouteFeatures/R/features.R
+++ RouteFeatures/R/features.R
@@ -1,3 +1,9 @@
+######################################################
+## All time units are seconds, all distance units   ##
+## are meters, all velocities are m/s, unless       ##
+## otherwise specified                              ##
+######################################################
+
 ## Each record in a route represents 1 second of time
 ## TotalTripTime takes a route and returns the number of
 ## records (which also represents the number of seconds)
@@ -20,4 +26,39 @@ StopFrequency <- function(route) {
 
 AverageStopDuration <- function(seconds) {
     mean(TimePerStop(seconds))
+}
+
+## TotalTimeStopped takes a route and returns the total time spent not traveling
+
+TotalTimeStopped <- function(route) {
+    length(SecondsStopped(DistancesTraveled(route)))
+}
+
+## TotalTimeTraveling takes a route and returns the total time spent traveling
+
+TotalTimeTraveling <- function(route) {
+    (nrow(route)-1) - TotalTimeStopped(route)
+}
+
+## AverageTravelSpeed takes a route and returns the average velocity per second traveling
+
+AverageTravelSpeed <- function(route) {
+    distances <- DistancesTraveled(route)
+    velocities <- distances[distances != 0]
+    round(mean(velocities), digits=5)
+}
+
+## TotalDistanceTraveled takes a route and returns the total distance traveled
+
+TotalDistanceTraveled <- function(route) {
+    round(sum(DistancesTraveled(route)), digits=5)
+}
+
+## MaxDistanceFromStart takes a route and returns the euclidean distance from the point
+## furthest from start
+
+MaxDistanceFromStart <- function(route) {
+    start <- data.frame(x=0, y=0)
+    distances.from.start <- sapply(1:nrow(route), function(i) DistanceTraveled(start,route[i,]))
+    round(max(distances.from.start), digits=5)
 }
--- RouteFeatures/tests/testthat/test_features.R
+++ RouteFeatures/tests/testthat/test_features.R
@@ -24,3 +24,38 @@ test_that("correct average stop duration is returned", {
     seconds <- c(1,2,3,7,8,9,10,15,16,17,18,19)
     expect_equal(AverageStopDuration(seconds),4)
 })
+
+context("Total Time Stopped")
+
+test_that("correct number of seconds returned as stopped", {
+    route <- data.frame(x=c(0,0,0,3,20,20,20), y=c(0,0,0,5,25,25,25))
+    expect_equal(TotalTimeStopped(route), 4)
+})
+
+context("Total Time Traveling")
+
+test_that("correct number of seconds returned as traveling", {
+    route <- data.frame(x=c(0,0,0,3,20,20,20), y=c(0,0,0,5,25,25,25))
+    expect_equal(TotalTimeTraveling(route), 2)
+})
+
+context("Average Travel Speed")
+
+test_that("correct mean travel velocity returned for route", {
+    route <- data.frame(x=c(0,0,0,3,20,20,20), y=c(0,0,0,5,25,25,25))
+    expect_equal(AverageTravelSpeed(route), 16.03988)
+})
+
+context("Total Distance Traveled")
+
+test_that("correct distance returned for route", {
+    route <- data.frame(x=c(0,0,0,3,20,20,20), y=c(0,0,0,5,25,25,25))
+    expect_equal(TotalDistanceTraveled(route), 32.07976)
+})
+
+context("Max Distance From Start")
+
+test_that("correct distance is returned as max from start for route", {
+    route <- data.frame(x=c(0,0,0,3,20,20,20), y=c(0,0,0,5,25,25,25))
+    expect_equal(MaxDistanceFromStart(route), 32.01562)
+})
